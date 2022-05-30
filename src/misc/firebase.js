116b6ba27d2ad220e375e51d2f29acac5359c790
Jheel Shah <jheelshah510@gmail.com>
2022-05-30 16:50:23 +0530

    firebase setup

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyD1T_-oRAfvKrDRnp5VHk0r6VoW4J15x54',
+  authDomain: 'chat-web-app-91211.firebaseapp.com',
+  projectId: 'chat-web-app-91211',
+  storageBucket: 'chat-web-app-91211.appspot.com',
+  messagingSenderId: '529430315338',
+  appId: '1:529430315338:web:45dc4191310faeaabb41b3',
+};
+
+const app = firebase.initializeApp(config);
