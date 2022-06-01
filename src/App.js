d14705288b13b1092cf0e48cbfe13604a38a8c75
Jheel Shah <jheelshah510@gmail.com>
2022-06-01 10:32:37 +0530

    setup firebase and sigIn page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Switch } from 'react-router-dom';
 
-import 'rsuite/dist/rsuite.min.css';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 import './styles/main.scss';
+import 'rsuite/dist/styles/rsuite-default.css';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return <div></div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
